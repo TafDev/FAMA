6d67bc4e60b4185490417c8f52ba332f30f5d166
TafDev <chombot@hotmail.com>
2017-07-16 00:06:23 +0100

    adding keys

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :deploy_to, '/home/fama/famaphotobooths'
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/application.yml, config/database.yml', 'config/secrets.yml')
 
 
 set :migration_role, :app
