6ae10ab871dbabe5572389128f2f4d54261f19e1
TafDev <chombot@hotmail.com>
2018-01-18 01:45:50 +0000

    keys

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,34 +1,20 @@
-require "rvm/capistrano"
-
-
 set :application, 'FAMA'
 set :repo_url, 'git@github.com:TafDev/FAMA.git'
 
-set :rvm_ruby_string, 'ruby-2.3.1p112' # Change to your ruby version
-set :rvm_type, :system # :user if RVM installed in $HOME
+set :deploy_to, '/home/deploy/famaphotobooths'
 
-set :ssh_options, {
-  forward_agent: true,
-  auth_methods: ["publickey"],
-  keys: ["#{Dir.home}/.ssh/fama.pem"]
-}
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/application.yml')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :keep_releases, 3
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
 
-  # Not needed if using capistrano-passenger gem/recipe
-  # desc 'Restart application'
-  # task :restart do
-  #   on roles(:app), in: :sequence, wait: 5 do
-  #     execute :touch, release_path.join('tmp/restart.txt')
-  #   end
-  # end
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
 
   after :publishing, 'deploy:restart'
   after :finishing, 'deploy:cleanup'
 end
-
-
-set :deploy_to, '/home/deploy/famaphotobooths'
