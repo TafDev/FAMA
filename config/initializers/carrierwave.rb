4cf5cfd221716d5659cb34bf7c2bdd61c767af91
TafDev <chombot@hotmail.com>
2018-01-25 22:20:32 +0000

    hiding stuff

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAIWSBCR2NK44RXRHQ',                        # required
-    aws_secret_access_key: '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',                        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],                        # required
+    aws_secret_access_key: ENV["AWS_SECRET"],                        # required
     region:                'eu-west-2',                  # optional, defaults to 'us-east-1'
     host:                  's3.eu-west-2.amazonaws.com',             # optional, defaults to nil
     # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
