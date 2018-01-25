f3c98f2ea3ba93d3d6e28c0ee96b29b58c8d6eda
TafDev <chombot@hotmail.com>
2018-01-25 22:39:06 +0000

    setting env variable

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],                        # required
-    aws_secret_access_key: ENV["AWS_SECRET"],                        # required
-    region:                'eu-west-2',                  # optional, defaults to 'us-east-1'
-    host:                  's3.eu-west-2.amazonaws.com',             # optional, defaults to nil
-    # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+    :provider              => 'AWS',
+    :aws_access_key_id     => ENV['AWS_ACCESS_KEY'],
+    :aws_secret_access_key => ENV['AWS_SECRET'],
+    :region                => 'eu-west-2',
+    :host                  => 's3.eu-west-2.amazonaws.com'
   }
   config.fog_directory  = 'famaphotobooths'                                   # required
   config.fog_public     = false                                                 # optional, defaults to true
